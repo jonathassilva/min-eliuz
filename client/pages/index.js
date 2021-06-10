4d6fb09a67073b1e8473d07908ea934b08ee8d7c
Jonathas Santos <jonathas.santos@meliuz.com.br>
2021-06-10 17:00:22 -0400

    Adicionando configurações do ExpressJS com BFF

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <p>Hello, world!</p>
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch('http://bff-service:3000')
+  const data = await res.text()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+export default function Home({ data }) {
+  return <p>{data}</p>
 }
